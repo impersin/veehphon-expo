e8facbdc660f591ee075b9501567e8da259d7bd9
impersin <closer124@gmail.com>
2018-12-06 22:20:00 -0800

    Implemented location permission

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    location: null,
+    errorMessage: null
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied'
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
   render() {
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
     return (
       <View style={styles.container}>
-        <Text>Voupon!!</Text>
+        <Text style={styles.paragraph}>Coupon 1</Text>
+        <Text style={styles.paragraph}>Coupon 2</Text>
+        <Text style={styles.paragraph}>Coupon 3</Text>
       </View>
     );
   }
@@ -14,8 +51,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1'
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center'
   }
 });

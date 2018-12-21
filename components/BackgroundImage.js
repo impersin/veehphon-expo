9b17dd16cec337a385c17b608a84d55008ade3c3
impersin <closer124@gmail.com>
2018-12-21 15:02:44 -0800

    Resized background image height

--- components/BackgroundImage.js
+++ components/BackgroundImage.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     // resizeMode: 'center',
     // position: 'absolute',
     // width: '100%',
-    height: 400
+    height: 300
     // justifyContent: 'center'
   }
 });

34fd007ffeff7099b43c0afc28bcd06ec2b9a451
impersin <closer124@gmail.com>
2018-12-24 15:36:10 -0800

    Implmented CouponCarousel component

---
+++ screens/CouponCarousel.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import {
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  Text
+} from 'react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons/';
+import { Constants } from 'expo';
+
+const { width } = Dimensions.get('window');
+const height = width * 0.8;
+
+export default class Carousel extends Component {
+  _goToPrevious() {
+    this.props.navigation.goBack();
+  }
+  render() {
+    // const { images } = this.props;
+    const coupon = this.props.navigation.state.params;
+    if (coupon) {
+      return (
+        <View style={styles.container}>
+          <View style={styles.topMenuWhite}>
+            <TouchableOpacity onPress={this._goToPrevious.bind(this)} style={styles.topMenuOne}>
+              <Ionicons name="ios-close" color={'white'} size={40} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._goToPrevious.bind(this)} style={styles.topMenuTwo}>
+              <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>Coupon</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1, backgroundColor: 'black' }}>
+            <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
+              <View style={{ justifyContent: 'center', flexWrap: 'wrap' }}>
+                <View style={{ width, padding: 10, alignItems: 'center' }}>
+                  <Text style={{ fontSize: 24, color: 'white', fontWeight: 'bold' }}>
+                    {coupon.dealName}
+                  </Text>
+                </View>
+                <Image
+                  style={{ width, height, resizeMode: 'contain' }}
+                  source={{ uri: coupon.photoUri }}
+                />
+                <View style={{ width, padding: 10 }}>
+                  <Text style={{ color: 'white' }}>{coupon.dealDetails}</Text>
+                </View>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      );
+    }
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: 'row',
+    // backgroundColor: 'black',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    // borderWidth: 1,
+    paddingTop: 0
+  },
+  topMenuWhite: {
+    display: 'flex',
+    alignSelf: 'stretch',
+    position: 'absolute',
+    top: 0,
+    height: 200,
+    zIndex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    backgroundColor: 'black'
+    // borderWidth: 1
+    // shadowOpacity: 0.5,
+    // shadowRadius: 4,
+    // shadowColor: 'black'
+  },
+  topMenuOne: {
+    flex: 1,
+    // borderWidth: 1,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  topMenuTwo: {
+    flex: 2,
+    // borderWidth: 1,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  topMenuThree: {
+    flex: 1,
+    // borderWidth: 1,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  scrollContainer: {
+    flex: 1,
+    // justifyContent: 'flex-end',
+    backgroundColor: 'red'
+  },
+  image: {
+    width,
+    resizeMode: 'contain'
+  }
+});

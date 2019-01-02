c4193405a1682c1fa3170a8cd0081a52854d5c77
impersin <closer124@gmail.com>
2019-01-02 15:08:35 -0800

    Removed image from coupon section.

--- components/Coupons.js
+++ components/Coupons.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import { Image, StyleSheet, ScrollView, Text, View, TouchableOpacity } from 'react-native';
-import { Ionicons, AntDesign } from '@expo/vector-icons/';
+import { MaterialIcons, AntDesign } from '@expo/vector-icons/';
+import BlockMenu from './BlockMenu';
 
 export default class Coupons extends React.Component {
   render() {
     return this.props.coupons.map((coupon, index) => {
       return (
-        // <View key={index} style={styles.couponWrapper}>
-        <TouchableOpacity
+        <View
           key={index}
-          style={styles.couponWrapper}
-          onPress={e => this.props.redirectToCarousel(coupon)}
+          style={{
+            borderBottomColor: '#ccc',
+            borderBottomWidth: 1
+          }}
         >
-          <View style={{ flex: 2 }}>
-            <Image
-              style={{
-                flex: 1,
-                width: 300,
-                height: 190,
-                resizeMode: 'contain'
-              }}
-              source={{ uri: coupon.photoUri }}
-            />
-          </View>
-          <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-            <Text style={styles.couponFont}>{coupon.dealName}</Text>
-          </View>
-        </TouchableOpacity>
-        // </View>
+          <TouchableOpacity
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              height: 40,
+              flexDirection: 'row',
+              borderBottomColor: '#ccc',
+              borderBottomWidth: 0.1
+            }}
+            onPress={e => this.props.redirectToCarousel(coupon)}
+          >
+            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+              <MaterialIcons name="redeem" color="#444" size={20} />
+              <Text style={styles.couponFont}>{coupon.dealName}</Text>
+            </View>
+            <View>
+              <AntDesign name="right" color="#444" size={20} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        // <TouchableOpacity
+        //   key={index}
+        //   style={styles.couponWrapper}
+        //   onPress={e => this.props.redirectToCarousel(coupon)}
+        // >
+        //   <View style={{ flex: 2 }}>
+        //     <Image
+        //       style={{
+        //         flex: 1,
+        //         width: 300,
+        //         height: 190,
+        //         resizeMode: 'contain'
+        //       }}
+        //       source={{ uri: coupon.photoUri }}
+        //     />
+        //     <AntDesign name="gift" />
+        //   </View>
+        //   <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
+        //     <Text style={styles.couponFont}>{coupon.dealName}</Text>
+        //   </View>
+        // </TouchableOpacity>
       );
     });
   }
@@ -37,14 +65,39 @@ const styles = StyleSheet.create({
   couponWrapper: {
     // width: 320,
     // height: 280,
-    marginRight: 10,
-    marginBottom: 20,
+    // marginRight: 10,
+    marginBottom: 10,
     borderWidth: 1,
     borderColor: '#ccc'
   },
   couponFont: {
     color: '#444',
     fontSize: 14,
-    fontWeight: 'bold'
+    marginLeft: 10
+    // fontWeight: 'bold'
+  },
+  blockWrapper: {
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row'
+  },
+  blockLeft: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  blockRight: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    marginRight: 5
+  },
+  titleContainer: {
+    justifyContent: 'center'
+  },
+  blockFont: {
+    color: '#444',
+    fontSize: 14
   }
 });

5e988cd4fe6bde93793d2bd7c3c73433fb03b579
impersin <closer124@gmail.com>
2019-01-10 12:19:12 -0800

    Wrapped Home component

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { Constants } from 'expo';
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import Home from './components/Home';
 
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 
 const store = configureStore();
-const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Home />
+        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+          <Home />
+        </View>
       </Provider>
     );
   }
